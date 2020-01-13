ee23bb3385a96b072516f1853e56d6728f1c2a33
prakash-dot-08 <prakash@dotworld.in>
2020-01-13 12:43:10 +0530

    Updates

---
+++ precache-manifest.ffc2e20f9dc1c15091c3d4c77eb2296e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125e993432b8918625a1cd636f76103d",
+    "url": "/dw-sw-prakashprabhakaran/my-app/index.html"
+  },
+  {
+    "revision": "6432a0029271689880a6",
+    "url": "/dw-sw-prakashprabhakaran/my-app/static/css/main.4d3a117d.chunk.css"
+  },
+  {
+    "revision": "ee45084456e82309d982",
+    "url": "/dw-sw-prakashprabhakaran/my-app/static/js/2.c1809de3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dw-sw-prakashprabhakaran/my-app/static/js/2.c1809de3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6432a0029271689880a6",
+    "url": "/dw-sw-prakashprabhakaran/my-app/static/js/main.d299de93.chunk.js"
+  },
+  {
+    "revision": "d19ae9c535c21dfda2da",
+    "url": "/dw-sw-prakashprabhakaran/my-app/static/js/runtime-main.047bfdff.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dw-sw-prakashprabhakaran/my-app/precache-manifest.ffc2e20f9dc1c15091c3d4c77eb2296e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dw-sw-prakashprabhakaran/my-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
